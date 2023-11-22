87cfb0a558f489692e44a0e14df09b6fce9247f2
ksnedeker1 <snedekek@oregonstate.edu>
2023-11-22 05:01:42 -0500

    Initial commit

---
+++ source/audio/effectors/Effector.cpp
@@ -0,0 +1,9 @@
+#include "Effector.h"
+
+Effector::Effector() {}
+
+Effector::~Effector() {}
+
+void Effector::processBlock(juce::AudioBuffer<float>& buffer, juce::MidiBuffer& midiMessages) { buffer.clear(); }
+
+const juce::String Effector::getName() const { return "Effector"; }
