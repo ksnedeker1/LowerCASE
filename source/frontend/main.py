77d58b0b76f967472bb4d73239af2d008da39bbf
ksnedeker1 <snedekek@oregonstate.edu>
2024-02-07 05:51:38 -0500

    Allow main.py to start the compiled backend

--- source/frontend/main.py
+++ source/frontend/main.py
@@ -1,3 +1,14 @@
+import subprocess
+import atexit
 from gui.mainwindowcontroller import launch
+from environment.local import PATH_TO_EXE
 
-launch('./test.json')
+
+def stop_backend(process):
+    process.terminate()
+
+
+backend = subprocess.Popen([PATH_TO_EXE])
+atexit.register(stop_backend, backend)
+
+launch('')
